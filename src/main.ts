0d53b4e29bb2ddc6febce8bb0e279b16085ac1b6
Aituar <aituufs@gmail.com>
2026-03-02 14:03:58 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ApplicationExceptionFilter } from './common/filters/appliction-exceptio
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3003',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
